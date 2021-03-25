7ab5435651101f630b72bba66d323d792e395d4e
raface <contact@rafagomes.com>
2021-03-25 17:27:37 -0400

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# python-kafka-demo
-Demo app to emulate a kafka producer and consumer using python
+# Kafka Python Demo
+
+## Description
+
+A demo app to emulate a kafka producer and consumer using python 
+
+## Installation
+The installation can be done via
+
+```bash
+pip install -r requirements.txt
+pip install -e .['dev']
+```
+
+## Usage
+
+The Python code can be run with the following
+
+```bash
+python_kafka_demo config.ini
+```
+Where
+* `config.ini`: Configuration file with all settings necessaries ot run the demo;
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    "aiven-client>=2.11.1,<3.0.0",
+    "kafka-python>=2.0.2,3.0.0",
+    "python-json-logger",
+]
+tests_require = ["nose2", "nose2-cov==1.0a4"]
+
+extras_requires = {"dev": install_requires + tests_require}
+
+entry_points = {"console_scripts": ["resize_building_images = resize_building_images:main"]}
+
+setup(
+    name="resize_building_images",
+    version="1.0.0",
+    description="",
+    author="github.com/raface",
+    author_email="contact@rafagomes.com",
+    packages=find_packages(),
+    zip_safe=False,
+    test_suite="nose2.collector.collector",
+    install_requires=install_requires,
+    entry_points=entry_points,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
